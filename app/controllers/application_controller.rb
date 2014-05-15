555607ca0b2c27f9f0a37bc5c56f1d14f5fa0134
Amirol Ahmad <9w2pdf@gmail.com>
2014-05-15 22:42:31 +0800

    add static page controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  ensure_security_headers(
-		:hsts => {:include_subdomains => true, :max_age => 20.years.to_i},
-		:x_frame_options => 'DENY',
-		:x_content_type_options = "nosniff",
-		:x_xss_protection = {:value => 1, :mode => 'block'},
-		:csp => false
-	)
+  #ensure_security_headers(
+	#	:hsts => {:include_subdomains => true, :max_age => 20.years.to_i},
+	#	:x_frame_options => 'DENY',
+	#	:x_content_type_options = "nosniff",
+	#	:x_xss_protection = {:value => 1, :mode => 'block'},
+	#	:csp => false
+	#)
 end
