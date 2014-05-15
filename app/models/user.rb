2cbfa5397eae0e5c23827b644d202164de7f7e95
Amirol Ahmad <9w2pdf@gmail.com>
2014-05-16 01:00:40 +0800

    option to login via username or password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, stretches: 20
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :authentication_keys => [:login], stretches: 20
+
+  validates :username, presence: true, uniqueness: { :case_sensitive => false }
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
