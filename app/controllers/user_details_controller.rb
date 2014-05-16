4fc3bea40441653612c88da88ccc3c5ddc273e66
AmirolAhmad <9w2pdf@gmail.com>
2014-05-16 17:22:32 +0800

    user details added with edit & update

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -1,12 +1,35 @@
 class UserDetailsController < ApplicationController
 
   def index
-  	# @userdetails = UserDetails.find(current_user.id)
-  	@userdetails = UserDetails.where("user_id = ?", current_user.id)
+  	@userdetails = UserDetails.find(current_user.id)
   end
 
   def edit
-  	@userdetails = UserDetails.find(current_user.id)
+  	# render plain: params[:id].inspect
+  	if params[:id].to_f == current_user.id
+  		@userdetails = UserDetails.find(current_user.id)
+  	else
+  		redirect_to root_path
+  	end
   end
+
+  def update
+  	@userdetails = UserDetails.find(current_user.id)
+  	if @userdetails.update_attributes(detail_params)
+      flash[:success] = "Profile updated"
+      redirect_to @userdetails
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+  	@userdetails = UserDetails.where("user_id = ?", current_user.id)
+  end
+
+  private
+	def detail_params
+	  params.require(:user_details).permit(:fname, :lname)
+	end
 
 end
--- app/models/user_details.rb
+++ app/models/user_details.rb
@@ -1,4 +1,7 @@
 class UserDetails < ActiveRecord::Base
 	belongs_to :user, :foreign_key => "user_id"
 
+	validates :fname, length: { minimum: 6 }
+	validates :lname, length: { minimum: 6 }
+
 end
